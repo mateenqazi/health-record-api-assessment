b042162a5e74ddb10062cf675500585e10626a68
Qazi Mateen <mateenqazi125@gmail.com>
2025-05-27 19:52:04 +0200

    feat: add admin interface and doctor-patient assignment

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, DoctorProfile, PatientProfile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'user_type', 'is_active', 'date_joined']
+    list_filter = ['user_type', 'is_active', 'date_joined']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('user_type', 'phone_number', 'date_of_birth')
+        }),
+    )
+
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'specialization', 'license_number', 'years_of_experience']
+    search_fields = ['user__username', 'user__email', 'specialization']
+
+@admin.register(PatientProfile)
+class PatientProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'assigned_doctor', 'blood_type', 'emergency_contact']
+    list_filter = ['assigned_doctor', 'blood_type']
+    search_fields = ['user__username', 'user__email']
+    
+    def save_model(self, request, obj, form, change):
+        # Trigger notification when doctor is assigned
+        if change and 'assigned_doctor' in form.changed_data and obj.assigned_doctor:
+            from notifications.tasks import send_patient_assignment_notification
+            send_patient_assignment_notification.delay(
+                obj.assigned_doctor.user.id,
+                obj.user.get_full_name()
+            )
+        super().save_model(request, obj, form, change)
+
+admin.site.register(User, CustomUserAdmin)
--- health_records/admin.py
+++ health_records/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import HealthRecord, DoctorComment
 
-# Register your models here.
+@admin.register(HealthRecord)
+class HealthRecordAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'title', 'record_type', 'visit_date', 'created_at']
+    list_filter = ['record_type', 'visit_date', 'created_at']
+    search_fields = ['patient__user__username', 'title', 'description']
+    readonly_fields = ['created_at', 'updated_at']
+
+@admin.register(DoctorComment)
+class DoctorCommentAdmin(admin.ModelAdmin):
+    list_display = ['health_record', 'doctor', 'is_private', 'created_at']
+    list_filter = ['is_private', 'created_at']
+    search_fields = ['health_record__title', 'doctor__user__username']
